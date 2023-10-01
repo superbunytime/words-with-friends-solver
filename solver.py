2f7125f41ace75892306ed4f83a63d2faef09e66
Eric White <led.ecodyswhite@gmail.com>
2023-10-01 14:14:20 -0700

    yes

--- solver.py
+++ solver.py
@@ -22,8 +22,28 @@ print(type(txt))
 # #     print("NOOOOOOOOOO")
 
 
-for word in txt:
-    if re.search("aardvark", word):
-        print("we found the word")
+# for word in txt:
+#     if re.search("aardvark", word):
+#         print(word)
 
+
+
+for word in txt:
+    if not re.findall("[^tcyvior]", word) and len(word) > 3 and len(word) < 5:
+        print(word)
 # uh hey this isn't done yet
+
+# let's test with the string on bun's stream
+
+# t g i h n f i g
+
+# correct words: gift hint high thin ting fight night thing gifting fighting
+# there may be more words than this, and this doesn't factor in fake letters
+# but it's an excellent starting point.
+
+# problem: it allows extra words; like if there's 1 o in the string it will return 
+# words that have two o's
+# also needs a better dictionary; this one puts out too many nonsense words
+# that would never be used in words with friends
+
+# problem: need a handler for mystery word
